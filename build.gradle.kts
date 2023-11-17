df60bbf4c98c855a9d03394a4b4a28f04ff0ce8d
Pavel Vashkel <jgugn1@gmail.com>
2023-11-17 23:08:24 +0100

    Add database dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,10 @@
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val exposed_version: String by project
+val postgres_version: String by project
+val hikaricp_version: String by project
+val dotenv_version: String by project
 
 plugins {
     kotlin("jvm") version "1.9.20"
@@ -23,10 +27,21 @@ repositories {
 }
 
 dependencies {
+    /* Server */
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
     implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
+    /* Serialization */
     implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+    /* Database */
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("org.postgresql:postgresql:$postgres_version")
+    implementation("com.zaxxer:HikariCP:$hikaricp_version")
+    /* Env */
+    implementation("io.github.cdimascio:dotenv-kotlin:$dotenv_version")
+    /* Logging */
     implementation("ch.qos.logback:logback-classic:$logback_version")
     /* Testing */
     testImplementation("io.ktor:ktor-server-tests-jvm")
