d7aa7affd3e68a17d464d2b4a5ee949b67245fde
Pavel Vashkel <jgugn1@gmail.com>
2023-11-21 19:18:23 +0100

    Extract auth routes to feature

---
+++ src/main/kotlin/com/knot/feature/auth/AuthRoutes.kt
@@ -0,0 +1,83 @@
+package com.knot.feature.auth
+
+import com.knot.feature.user.User
+import com.knot.feature.user.UsersRepository
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.request.*
+import io.ktor.server.resources.post
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Route.authRoutes(
+    usersRepository: UsersRepository,
+    jwtService: JwtService,
+    hashFunction: (String) -> String,
+) {
+    post<AuthResource.SignUp> {
+        lateinit var signUpDto: SignUpDto
+
+        try {
+            signUpDto = call.receive<SignUpDto>()
+        } catch (e: ContentTransformationException) {
+            application.log.error("Failed to process request", e)
+            call.respond(HttpStatusCode.BadRequest, "Incomplete data")
+        }
+
+        if (signUpDto.email.isBlank() || signUpDto.displayName.isBlank() || signUpDto.password.isBlank()) {
+            call.respond(HttpStatusCode.BadRequest, "Incomplete data")
+        }
+
+        if (signUpDto.password.length < 8) {
+            call.respond(HttpStatusCode.BadRequest, "Password is too short")
+        }
+
+        try {
+            val newUser: User? = usersRepository.createUser(
+                email = signUpDto.email,
+                displayName = signUpDto.displayName,
+                passwordHash = hashFunction.invoke(signUpDto.password),
+            )
+
+            newUser?.run {
+                val accessTokenDto = AccessTokenDto(token = jwtService.generateToken(newUser))
+                call.respond(HttpStatusCode.Created, accessTokenDto)
+            } ?: run {
+                application.log.error("Failed to create user")
+                call.respond(HttpStatusCode.BadRequest, "Failed to create user")
+            }
+        } catch (e: Throwable) {
+            application.log.error("Failed to create user", e)
+            call.respond(HttpStatusCode.BadRequest, "Failed to create user")
+        }
+    }
+    post<AuthResource.SignIn> {
+        lateinit var signInDto: SignInDto
+
+        try {
+            signInDto = call.receive<SignInDto>()
+        } catch (e: ContentTransformationException) {
+            application.log.error("Failed to process request", e)
+            call.respond(HttpStatusCode.BadRequest, "Incomplete data")
+        }
+
+        if (signInDto.email.isBlank() || signInDto.password.isBlank()) {
+            call.respond(HttpStatusCode.BadRequest, "Incomplete data")
+        }
+
+        val passwordHash = hashFunction(signInDto.password)
+        try {
+            val user: User? = usersRepository.findUser(signInDto.email)
+            if (passwordHash == user?.passwordHash) {
+                val accessTokenDto = AccessTokenDto(token = jwtService.generateToken(user))
+                call.respond(accessTokenDto)
+            } else {
+                application.log.error("Failed to create user")
+                call.respond(HttpStatusCode.Unauthorized)
+            }
+        } catch (e: Throwable) {
+            application.log.error("Failed to create user", e)
+            call.respond(HttpStatusCode.BadRequest, "Failed to sign in")
+        }
+    }
+}
--- src/main/kotlin/com/knot/plugins/Routing.kt
+++ src/main/kotlin/com/knot/plugins/Routing.kt
@@ -1,17 +1,9 @@
 package com.knot.plugins
 
 import com.knot.feature.auth.JwtService
-import com.knot.feature.auth.AccessTokenDto
-import com.knot.feature.auth.SignInDto
-import com.knot.feature.auth.SignUpDto
-import com.knot.feature.user.User
+import com.knot.feature.auth.authRoutes
 import com.knot.feature.user.UsersRepository
-import com.knot.feature.auth.AuthResource
-import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.request.*
-import io.ktor.server.resources.post
-import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting(
@@ -20,75 +12,6 @@ fun Application.configureRouting(
     hashFunction: (String) -> String,
 ) {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
-        }
-        // Auth
-        post<AuthResource.SignUp> {
-            lateinit var signUpDto: SignUpDto
-
-            try {
-                signUpDto = call.receive<SignUpDto>()
-            } catch (e: ContentTransformationException) {
-                application.log.error("Failed to process request", e)
-                call.respond(HttpStatusCode.BadRequest, "Incomplete data")
-            }
-
-            if (signUpDto.email.isBlank() || signUpDto.displayName.isBlank() || signUpDto.password.isBlank()) {
-                call.respond(HttpStatusCode.BadRequest, "Incomplete data")
-            }
-
-            if (signUpDto.password.length < 8) {
-                call.respond(HttpStatusCode.BadRequest, "Password is too short")
-            }
-
-            try {
-                val newUser: User? = usersRepository.createUser(
-                    email = signUpDto.email,
-                    displayName = signUpDto.displayName,
-                    passwordHash = hashFunction.invoke(signUpDto.password),
-                )
-
-                newUser?.run {
-                    val accessTokenDto = AccessTokenDto(token = jwtService.generateToken(newUser))
-                    call.respond(HttpStatusCode.Created, accessTokenDto)
-                } ?: run {
-                    application.log.error("Failed to create user")
-                    call.respond(HttpStatusCode.BadRequest, "Failed to create user")
-                }
-            } catch (e: Throwable) {
-                application.log.error("Failed to create user", e)
-                call.respond(HttpStatusCode.BadRequest, "Failed to create user")
-            }
-        }
-        post<AuthResource.SignIn> {
-            lateinit var signInDto: SignInDto
-
-            try {
-                signInDto = call.receive<SignInDto>()
-            } catch (e: ContentTransformationException) {
-                application.log.error("Failed to process request", e)
-                call.respond(HttpStatusCode.BadRequest, "Incomplete data")
-            }
-
-            if (signInDto.email.isBlank() || signInDto.password.isBlank()) {
-                call.respond(HttpStatusCode.BadRequest, "Incomplete data")
-            }
-
-            val passwordHash = hashFunction(signInDto.password)
-            try {
-                val user: User? = usersRepository.findUser(signInDto.email)
-                if (passwordHash == user?.passwordHash) {
-                    val accessTokenDto = AccessTokenDto(token = jwtService.generateToken(user))
-                    call.respond(accessTokenDto)
-                } else {
-                    application.log.error("Failed to create user")
-                    call.respond(HttpStatusCode.Unauthorized)
-                }
-            } catch (e: Throwable) {
-                application.log.error("Failed to create user", e)
-                call.respond(HttpStatusCode.BadRequest, "Failed to sign in")
-            }
-        }
+        authRoutes(usersRepository, jwtService, hashFunction)
     }
 }
