bbb3b75b96c1ac2978294bf75938839a73c38b13
Pavel Vashkel <jgugn1@gmail.com>
2023-11-24 22:03:45 +0100

    Configure notes validations

--- src/main/kotlin/com/knot/plugins/ConfigureRequestValidation.kt
+++ src/main/kotlin/com/knot/plugins/ConfigureRequestValidation.kt
@@ -1,6 +1,7 @@
 package com.knot.plugins
 
 import com.knot.feature.auth.authValidations
+import com.knot.feature.note.noteValidations
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.requestvalidation.RequestValidation
@@ -8,5 +9,6 @@ import io.ktor.server.plugins.requestvalidation.RequestValidation
 fun Application.configureRequestValidation() {
     install(RequestValidation) {
         authValidations()
+        noteValidations()
     }
 }
