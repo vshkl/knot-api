09848e75821fe70bd25bebec62ae3e25614cfb4d
Pavel Vashkel <jgugn1@gmail.com>
2023-11-24 13:17:44 +0100

    Add ArrowKt dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,18 @@
-val ktor_version: String by project
-val kotlin_version: String by project
-val logback_version: String by project
-val exposed_version: String by project
-val postgres_version: String by project
-val hikaricp_version: String by project
-val dotenv_version: String by project
-val detekt_version: String by project
+val arrowVersion: String by project
+val detektVersion: String by project
+val dotenvVersion: String by project
+val exposedVersion: String by project
+val hikaricpVersion: String by project
+val kotlinVersion: String by project
+val ktorVersion: String by project
+val logbackVersion: String by project
+val postgresVersion: String by project
 
 plugins {
     kotlin("jvm") version "1.9.20"
     kotlin("plugin.serialization") version "1.9.20"
     id("io.ktor.plugin") version "2.3.6"
-    id("io.gitlab.arturbosch.detekt") version ("1.23.3")
+    id("io.gitlab.arturbosch.detekt") version "1.23.3"
 }
 
 group = "com.knot"
@@ -25,7 +26,7 @@ application {
 }
 
 detekt {
-    toolVersion = detekt_version
+    toolVersion = detektVersion
     config.setFrom(file("config/detekt/detekt.yml"))
     buildUponDefaultConfig = true
 }
@@ -38,27 +39,31 @@ dependencies {
     /* Server */
     implementation("io.ktor:ktor-server-core-jvm")
     implementation("io.ktor:ktor-server-netty-jvm")
-    implementation("io.ktor:ktor-server-content-negotiation:$ktor_version")
-    implementation("io.ktor:ktor-server-auth:$ktor_version")
-    implementation("io.ktor:ktor-server-auth-jwt:$ktor_version")
-    implementation("io.ktor:ktor-server-resources:$ktor_version")
+    implementation("io.ktor:ktor-server-content-negotiation:$ktorVersion")
+    implementation("io.ktor:ktor-server-auth:$ktorVersion")
+    implementation("io.ktor:ktor-server-auth-jwt:$ktorVersion")
+    implementation("io.ktor:ktor-server-resources:$ktorVersion")
     /* Serialization */
-    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktor_version")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
     /* Database */
-    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
-    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
-    implementation("org.postgresql:postgresql:$postgres_version")
-    implementation("com.zaxxer:HikariCP:$hikaricp_version")
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.postgresql:postgresql:$postgresVersion")
+    implementation("com.zaxxer:HikariCP:$hikaricpVersion")
     /* Env */
-    implementation("io.github.cdimascio:dotenv-kotlin:$dotenv_version")
+    implementation("io.github.cdimascio:dotenv-kotlin:$dotenvVersion")
     /* Logging */
-    implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+    /* ArrowKt */
+    implementation(platform("io.arrow-kt:arrow-stack:$arrowVersion"))
+    implementation("io.arrow-kt:arrow-core")
+    implementation("io.arrow-kt:arrow-fx-coroutines")
     /* Detekt */
-    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.23.3")
+    detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:$detektVersion")
     /* Testing */
     testImplementation("io.ktor:ktor-server-tests-jvm")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
     testImplementation("io.ktor:ktor-server-test-host-jvm:2.3.6")
-    testImplementation("io.ktor:ktor-client-content-negotiation:$ktor_version")
+    testImplementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
 }
