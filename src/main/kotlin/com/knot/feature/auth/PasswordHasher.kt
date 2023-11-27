9ff60f3a0aae9920b131c5110432eb1d55b959d6
Pavel Vashkel <jgugn1@gmail.com>
2023-11-27 12:42:28 +0100

    Refactor `PasswordHasher` implementation

--- src/main/kotlin/com/knot/feature/auth/PasswordHasher.kt
+++ src/main/kotlin/com/knot/feature/auth/PasswordHasher.kt
@@ -1,28 +1,40 @@
 package com.knot.feature.auth
 
-import io.github.cdimascio.dotenv.dotenv
 import io.ktor.util.hex
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
 
-object PasswordHasher {
+class PasswordHasherImpl(
+    private val secretKet: String,
+) : PasswordHasher {
 
-    private val dotenv by lazy { dotenv() }
+    private val key by lazy { SecretKeySpec(hex(secretKet), ALGORITHM) }
 
-    private val hashKey = hex(dotenv["SECRET_KEY"])
-    private val hmacKey = SecretKeySpec(hashKey, "HmacSHA1")
-
-    /**
-     * Hashes a given password using the HmacSHA1 algorithm.
-     *
-     * @param password the password to be hashed
-     * @return the hashed password as a hexadecimal string
-     */
-    fun hash(password: String): String {
-        val hmac = Mac.getInstance("HmacSHA1")
-            .apply { init(hmacKey) }
-        return hmac
+    private val mac by lazy {
+        Mac.getInstance(ALGORITHM)
+            .apply { init(key) }
+    }
+    override fun hash(password: String): String {
+        return mac
             .doFinal(password.toByteArray(Charsets.UTF_8))
             .run(::hex)
     }
+
+    private companion object {
+        const val ALGORITHM = "HmacSHA1"
+    }
+}
+
+/**
+ * This interface defines the contract for a password hasher.
+ */
+interface PasswordHasher {
+
+    /**
+     * Calculates the hash of the given password.
+     *
+     * @param password The password to be hashed.
+     * @return The calculated hash of the password.
+     */
+    fun hash(password: String): String
 }
